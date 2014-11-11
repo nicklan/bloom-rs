5261f8639ebf7214630af9ab829e241838989874
Nick Lanham <nick@afternight.org>
2014-11-11 15:57:43 -0800

    Simplify a lot, and just use two SipHashers

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,10 @@
 //! use bloom::BloomFilter;
 //! let expected_num_items = 1000;
 //! let false_positive_rate = 0.01;
-//! /* make a bloomfilter for ints */
-//! let mut filter:BloomFilter<int,int> = BloomFilter::with_rate(false_positive_rate,expected_num_items);
-//! filter.insert(&1);
-//! filter.contains(&1); /* true */
-//! filter.contains(&2); /* false */
+//! let mut filter:BloomFilter = BloomFilter::with_rate(false_positive_rate,expected_num_items);
+//! filter.insert(&1i);
+//! filter.contains(&1i); /* true */
+//! filter.contains(&2i); /* false */
 //! ```
 //!
 //! # False Positive Rate
@@ -19,15 +18,6 @@
 //! return a false positive.  Higher values will lead to smaller (but
 //! more inaccurate) filters.
 //!
-//! # Note on type parameters
-//!
-//! Since at least two independent hash functions are needed for a
-//! bloom filter, it is currently necessary to specify two types to
-//! the the `new/with_hasher` functions.  These types *must* always be
-//! the same.  (i.e. `BloomFilter<f32,f32>`) This is because a type
-//! like `T: Hash<SipState> + Hash<Djb2State>` does not seem to be
-//! currently possible. Therefore, the two requirements are split into
-//! the two types and transmuted.
 
 #![feature(default_type_params)]
 #![license = "GPL2"]
@@ -35,58 +25,39 @@ extern crate collections;
 extern crate core;
 
 use collections::Bitv;
-use std::hash::{hash,Hash,Hasher};
+use std::hash::{Hash,Hasher,RandomSipHasher};
 use std::cmp::{min,max};
 
-use djb2::{Djb2Hasher,Djb2State};
-mod djb2;
 
-
-pub struct BloomFilter<T, U, H = Djb2Hasher> {
+pub struct BloomFilter {
     bits: Bitv,
     num_hashes: uint,
-    hasher: H,
+    h1: RandomSipHasher,
+    h2: RandomSipHasher,
 }
 
-impl<T: Hash, U: Hash<Djb2State>> BloomFilter<T,U,Djb2Hasher> {
-    /// create a BloomFilter that expectes to hold
-    /// `expected_num_items`.  The filter will be sized to have a
-    /// false positive rate of the value specified in `rate` and
-    /// will use a Djb2 hasher.
-    pub fn with_rate(rate: f32, expected_num_items: uint) -> BloomFilter<T,U,Djb2Hasher> {
-        let bits = needed_bits(rate,expected_num_items);
-        BloomFilter::new(bits,optimal_num_hashes(bits,expected_num_items))
-    }
-
+impl BloomFilter {
     /// Create a new BloomFilter with the specified number of bits,
-    /// hashes, and a Djb2 hasher.
-    pub fn new(num_bits: uint, num_hashes: uint) -> BloomFilter<T,U,Djb2Hasher> {
-        BloomFilter::with_hasher(num_bits,num_hashes,Djb2Hasher)
-    }
-}
-
-impl<T: Hash, U: Hash<S>, S, H:Hasher<S>> BloomFilter<T,U,H> {
-    /// create a BloomFilter that expectes to hold
-    /// `expected_num_items`.  The filter will be sized to have a
-    /// false positive rate of the value specified in `rate` and
-    /// will use the specified hasher
-    pub fn with_rate_and_hasher(rate: f32, expected_num_items: uint, hasher: H) -> BloomFilter<T,U,H> {
-        let bits = needed_bits(rate,expected_num_items);
-        BloomFilter::with_hasher(bits,optimal_num_hashes(bits,expected_num_items),hasher)
-    }
-
-    /// Create a BloomFilter with the specified number of bits,
-    /// hashes, and the specified hasher.
-    pub fn with_hasher(num_bits: uint, num_hashes: uint, hasher: H) -> BloomFilter<T,U,H> {
+    /// and hashes
+    pub fn with_size(num_bits: uint, num_hashes: uint) -> BloomFilter {
         BloomFilter {
             bits: Bitv::with_capacity(num_bits, false),
             num_hashes: num_hashes,
-            hasher: hasher,
+            h1: RandomSipHasher::new(),
+            h2: RandomSipHasher::new(),
         }
     }
 
+    /// create a BloomFilter that expectes to hold
+    /// `expected_num_items`.  The filter will be sized to have a
+    /// false positive rate of the value specified in `rate`.
+    pub fn with_rate(rate: f32, expected_num_items: uint) -> BloomFilter {
+        let bits = needed_bits(rate,expected_num_items);
+        BloomFilter::with_size(bits,optimal_num_hashes(bits,expected_num_items))
+    }
+
     /// Insert item into this bloomfilter
-    pub fn insert(& mut self,item: &T) {
+    pub fn insert<T: Hash>(& mut self,item: &T) {
         for h in self.get_hashes(item).iter() {
             let idx = (h % self.bits.len() as u64) as uint;
             self.bits.set(idx,true)
@@ -96,7 +67,7 @@ impl<T: Hash, U: Hash<S>, S, H:Hasher<S>> BloomFilter<T,U,H> {
     /// Check if the item has been inserted into this bloom filter.
     /// This function can return false positives, but no false
     /// negatives.
-    pub fn contains(&self, item: &T) -> bool {
+    pub fn contains<T: Hash>(&self, item: &T) -> bool {
         for h in self.get_hashes(item).iter() {
             let idx = (h % self.bits.len() as u64) as uint;
             if !self.bits.get(idx) {
@@ -107,14 +78,10 @@ impl<T: Hash, U: Hash<S>, S, H:Hasher<S>> BloomFilter<T,U,H> {
     }
 
 
-    fn get_hashes(&self, item: &T) -> Vec<u64> {
+    fn get_hashes<T: Hash>(&self, item: &T) -> Vec<u64> {
         let mut vec = Vec::with_capacity(self.num_hashes);
-        let h1 = hash(item);
-        let h2 =
-            unsafe {
-                let u:&U = std::mem::transmute(item);
-                self.hasher.hash(u)
-            };
+        let h1 = self.h1.hash(item);
+        let h2 = self.h2.hash(item);
         for i in range(0,self.num_hashes) {
             vec.push((h1+i as u64 *h2))
         }
@@ -152,10 +119,10 @@ mod test_bloom {
 
     #[test]
     fn simple() {
-        let mut b:BloomFilter<uint,uint> = BloomFilter::with_rate(0.01,100);
-        b.insert(&1);
-        assert!(b.contains(&1));
-        assert!(!b.contains(&2));
+        let mut b:BloomFilter = BloomFilter::with_rate(0.01,100);
+        b.insert(&1i);
+        assert!(b.contains(&1i));
+        assert!(!b.contains(&2i));
     }
 
     #[test]
@@ -168,7 +135,7 @@ mod test_bloom {
         let hashes = optimal_num_hashes(bits,cnt);
         assert_eq!(hashes, 7);
 
-        let mut b:BloomFilter<int,int> = BloomFilter::new(bits,hashes);
+        let mut b:BloomFilter = BloomFilter::with_rate(rate,cnt);
         let mut set:HashSet<int> = HashSet::new();
         let mut rng = rand::task_rng();
 
